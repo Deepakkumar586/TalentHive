30181de85694c18160cb2ce379a224ad055418af
Deepak <aryanrajj1234@gmail.com>
2024-12-18 19:31:05 +0530

    add url of Deploy

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT = "http://localhost:2000/api/user";
-export const JOB_API_END_POINT = "http://localhost:2000/api/job";
+export const USER_API_END_POINT = "https://talenthive-kkxx.onrender.com/api/user";
+export const JOB_API_END_POINT = "https://talenthive-kkxx.onrender.com/api/job";
 export const APPLICATION_API_END_POINT =
-  "http://localhost:2000/api/application";
-export const COMPANY_API_END_POINT = "http://localhost:2000/api/company";
+  "https://talenthive-kkxx.onrender.com/api/application";
+export const COMPANY_API_END_POINT = "https://talenthive-kkxx.onrender.com/api/company";
